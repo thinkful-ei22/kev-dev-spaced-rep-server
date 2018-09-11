a065eb74af7077189c64b5958c3a1856cd2de9ee
dghero <devingreyhero@gmail.com>
2018-09-11 09:24:54 -0700

    Modify dummy data

--- routers/words.js
+++ routers/words.js
@@ -6,34 +6,40 @@ const router = express.Router();
 
 const someWords = [
   {
+    id: '5b745285ce8de04fbwatashi',
+    untranslated: 'わたし',
+    phonetic: 'watashi',
+    translation: ['me', 'I', 'myself']
+  },
+  {
     id: '5b745285ce8de04fb0d85one',
     untranslated: 'いち',
     phonetic: 'ichi',
-    translation: 'one'
+    translation: ['one']
   },
   {
     id: '5b64bd1bc377b892b4c24two',
     untranslated: 'に',
     phonetic: 'ni',
-    translation: 'two'
+    translation: ['two']
   },
   {
     id: '5b7460cf20c593464c7three',
     untranslated: 'さん',
     phonetic: 'san',
-    translation: 'three'
+    translation: ['three']
   },
   {
     id: '5b7460d920c593464c7dfour',
     untranslated: 'よん',
     phonetic: 'yon',
-    translation: 'four'
+    translation: ['four']
   },
   {
     id: '5b7460d920c593464c7hfive',
     untranslated: 'ご',
     phonetic: 'go',
-    translation: 'five'
+    translation: ['five']
   },
 
 ];
