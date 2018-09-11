726c2967df7cb3fa8cc3d5a5a458aa69d6f34a02
dghero <devingreyhero@gmail.com>
2018-09-11 08:23:50 -0700

    Add id's to dummy data for test purposes

--- routers/words.js
+++ routers/words.js
@@ -6,26 +6,31 @@ const router = express.Router();
 
 const someWords = [
   {
+    id: '5b745285ce8de04fb0d85one',
     word: 'いち',
     phonetic: 'ichi',
     translation: 'one'
   },
   {
+    id: '5b64bd1bc377b892b4c24two',
     word: 'に',
     phonetic: 'ni',
     translation: 'two'
   },
   {
+    id: '5b7460cf20c593464c7three',
     word: 'さん',
     phonetic: 'san',
     translation: 'three'
   },
   {
+    id: '5b7460d920c593464c7dfour',
     word: 'よん',
     phonetic: 'yon',
     translation: 'four'
   },
   {
+    id: '5b7460d920c593464c7hfive',
     word: 'ご',
     phonetic: 'go',
     translation: 'five'
