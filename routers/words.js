4d0455fe8c8619edc9b5e84a47f954fea575ab38
dghero <devingreyhero@gmail.com>
2018-09-12 15:22:49 -0700

    Cleanup words endpoint

--- routers/words.js
+++ routers/words.js
@@ -4,47 +4,6 @@ const Word = require('../db/models/wordSchema');
 
 const router = express.Router();
 
-//temp dummy data
-const someWords = [
-  {
-    id: '5b745285ce8de04fbwatashi',
-    untranslated: 'わたし',
-    phonetic: 'watashi',
-    translation: ['me', 'I', 'myself']
-  },
-  {
-    id: '5b745285ce8de04fb0d85one',
-    untranslated: 'いち',
-    phonetic: 'ichi',
-    translation: ['one']
-  },
-  {
-    id: '5b64bd1bc377b892b4c24two',
-    untranslated: 'に',
-    phonetic: 'ni',
-    translation: ['two']
-  },
-  {
-    id: '5b7460cf20c593464c7three',
-    untranslated: 'さん',
-    phonetic: 'san',
-    translation: ['three']
-  },
-  {
-    id: '5b7460d920c593464c7dfour',
-    untranslated: 'よん',
-    phonetic: 'yon',
-    translation: ['four']
-  },
-  {
-    id: '5b7460d920c593464c7hfive',
-    untranslated: 'ご',
-    phonetic: 'go',
-    translation: ['five']
-  },
-
-];
-
 router.get('/random', (req, res, next)=>{
   
   Word.find()
@@ -64,13 +23,8 @@ router.get('/random', (req, res, next)=>{
 router.get('/:id', (req, res, next)=>{
   const id = req.params.id;
 
-  const word = someWords.filter(word => word.id === id);
-
-  console.log('hello?');
-
   Word.findById(id)
     .then(results =>{
-      console.log('help', results);
       res.json(results);
     })
     .catch(err =>{
